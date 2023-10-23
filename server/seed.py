a2204cb8019f9c7dbe836c71f6cf0bdf6e553734
Matthew <matthew.stewart827@gmail.com>
2023-10-23 08:28:46 -0500

    create random seeds for seed.py

--- server/seed.py
+++ server/seed.py
@@ -9,10 +9,66 @@ from faker import Faker
 # Local imports
 from app import app
 from models import db
-from models import DailyHabit, User, HabitEntry
+from model import User, HabitEntry, DailyHabit, WeeklyHabit, MonthlyHabit
+
+fake = Faker()
+
+def create_habits():
+    habits = []
+    for _ in range(20):
+        h = DailyHabit(
+            name = fake.name(),
+        )
+        habits.append(h)
+    return habits
+
+
+def create_users():
+    users = []
+    for _ in range(5):
+        u = User(
+            username = fake.user_name(),
+        )
+        users.append(u)
+    return users
+
+
+def create_entries(users, habits):
+    entries = []
+
+    for _ in range(20):
+        e = HabitEntry(
+            name = fake.name(),
+            dailyHabit_id = rc(habits).id,
+            user_id = rc(users).id
+        )
+        entries.append(e)
+    return entries
+
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        User.query.delete()
+        HabitEntry.query.delete()
+        DailyHabit.query.delete()
+        WeeklyHabit.query.delete()
+        MonthlyHabit.query.delete()
+
+        print("seeding DailyHabits...")
+        habits = create_habits()
+        db.session.add(habits)
+        db.session.commit()
+
+        print("seeding users...")
+        users = create_users()
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("seeding HabitEntries...")
+        entries = create_entries(users, habits)
+        db.sesison.add_all(entries)
+        db.session.commit()
+
+        print("Done seeding!")
